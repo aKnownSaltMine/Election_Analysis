32a6f5477e14028fd0829df385d23c73b72bfea0
aKnownSaltMine <ctcarpenter72@yahoo.com>
2022-02-26 15:34:18 -0600

    PyPoll.py with txt file code

--- PyPoll.py
+++ PyPoll.py
@@ -56,25 +56,42 @@ with open(file_to_load) as election_data:
         # Add a vote to the candidate's count.
         candidate_votes[candidate_name] += 1
 
-# a for loop to iterate through each candidate name and calculate their total percenetage of votes
-for candidate_name in candidate_votes:
-    # Grabbing the total amount of votes for each candidate
-    votes = candidate_votes[candidate_name]
-    # Calculating the percentage 
-    vote_percentage = (float(votes) / float(total_votes)) * 100
+# Save the results to our txt file
+with open(file_to_save, "w") as txt_file:
+    # Print the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the txt file
+    txt_file.write(election_results)
+        
 
-    print(f"{candidate_name}: recieved {vote_percentage:.1f}% ({votes:,})\n")
+    # a for loop to iterate through each candidate name and calculate their total percenetage of votes
+    for candidate_name in candidate_votes:
+        # Grabbing the total amount of votes for each candidate
+        votes = candidate_votes[candidate_name]
+        # Calculating the percentage 
+        vote_percentage = (float(votes) / float(total_votes)) * 100
 
-    #Determine if the votes are greater than the winning count.
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        # If true then update the values of the winning variables
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winning_candidate = candidate_name
-winning_candidate_summary = (
-    f"----------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"----------------------\n")
-print(winning_candidate_summary)
+        candidate_results = (f"{candidate_name}: recieved {vote_percentage:.1f}% ({votes:,})\n")
+        # Print results to terminal
+        print(candidate_results)
+        # Write results to txt file
+        txt_file.write(candidate_results)
+
+        #Determine if the votes are greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then update the values of the winning variables
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+    winning_candidate_summary = (
+        f"----------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"----------------------\n")
+    # print(winning_candidate_summary)
