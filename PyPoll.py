62334c8d97c2836b7e39a39d22f9a13d2147f295
aKnownSaltMine <ctcarpenter72@yahoo.com>
2022-02-26 15:19:05 -0600

    PyPoll.py Updated

--- PyPoll.py
+++ PyPoll.py
@@ -19,6 +19,13 @@ total_votes = 0
 
 # Candidate list
 candidate_options = []
+#variable to track the candidate votes
+candidate_votes = {}
+
+# Winning Canidate and winning count tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0.0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -43,4 +50,31 @@ with open(file_to_load) as election_data:
             # Add the candidate name to the candidate list.
             candidate_options.append(candidate_name)
 
-print(candidate_options)
+            # Tracking the candidate's vote count
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to the candidate's count.
+        candidate_votes[candidate_name] += 1
+
+# a for loop to iterate through each candidate name and calculate their total percenetage of votes
+for candidate_name in candidate_votes:
+    # Grabbing the total amount of votes for each candidate
+    votes = candidate_votes[candidate_name]
+    # Calculating the percentage 
+    vote_percentage = (float(votes) / float(total_votes)) * 100
+
+    print(f"{candidate_name}: recieved {vote_percentage:.1f}% ({votes:,})\n")
+
+    #Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # If true then update the values of the winning variables
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+winning_candidate_summary = (
+    f"----------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"----------------------\n")
+print(winning_candidate_summary)
